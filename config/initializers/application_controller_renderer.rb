a54a0aee15b567f7497b659ec9f119124dd1aa79
tanakashun <michael021212@gmail.com>
2019-12-09 09:51:42 +0000

    [Finish]5-3, [Start]5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6c4f445e07888c9403c19852f1de62401830d54def1bb63aca2fc19615c25d457be7d619f2b06415b966847cdb09a1a0b7d052e6bf9dc6a5f84db44ce75afcb0'
