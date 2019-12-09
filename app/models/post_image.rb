865f9cc0f7009b6a779f988ab358f95611d6af91
tanakashun <michael021212@gmail.com>
2019-12-09 10:09:04 +0000

    [Finish]5-4

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,8 @@ class PostImage < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :post_comments, dependent: :destroy
+  validates :shop_name, presence:true
+  validates :image, presence: true
   has_many :favorites, dependent: :destroy
     def favorited_by?(user)
       favorites.where(user_id: user.id).exists?
